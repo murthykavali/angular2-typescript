23e7d07cd9e3863a32a2a90e355ed0ee8ab58489
Murthy Kavali <murthykavali@gmail.com>
2016-05-01 13:27:51 +0530

    Angular2 Typescript First Commit

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by mkavali on 5/1/16.
+ */
+
+import {Component} from "angular2/core";
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First and Second Angular 2 App</h1>'
+})
+export class AppComponent {
+}
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by mkavali on 5/1/16.
+ */
+
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
